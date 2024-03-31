074c702f91b87c7bf80a2b6cdf7eaefdf0d527be
alexpetrovan2001 <alexpetrovan2001@gmail.com>
2024-03-31 16:07:08 +0300

    More test cases added to test add student feature

--- src/test/java/org/example/TestStudents.java
+++ src/test/java/org/example/TestStudents.java
@@ -25,4 +25,43 @@ public class TestStudents {
         repo.save(new Student("1", "John", 109));
         assertNull("Verifying student with invalid group is not added", repo.findOne("1"));
     }
+
+    @Test
+    public void testAddStudentInvalidId(){
+        repo.save(new Student("", "Johnny", 123));
+        assertNull("Verifying student with invalid group", repo.findOne(""));
+    }
+
+    @Test
+    public void testAddStudentInvalidName(){
+        repo.save(new Student("1", "", 123));
+        assertNull("Verifying student with invalid group", repo.findOne("1"));
+    }
+
+    @Test public void testAddStudentValidGroupBoundaries(){
+        repo.save(new Student("1", "Johnny 111", 111));
+        Student addedStudent = repo.findOne("1");
+        assertEquals("Verifying student is added", "Johnny 111", addedStudent.getNume());
+
+        repo.save(new Student("2", "Johnny 112", 112));
+        addedStudent = repo.findOne("2");
+        assertEquals("Verifying student is added", "Johnny 112", addedStudent.getNume());
+
+        repo.save(new Student("3", "Johnny 936", 936));
+        addedStudent = repo.findOne("3");
+        assertEquals("Verifying student is added", "Johnny 936", addedStudent.getNume());
+
+        repo.save(new Student("4", "Johnny 937", 937));
+        addedStudent = repo.findOne("4");
+        assertEquals("Verifying student is added", "Johnny 937", addedStudent.getNume());
+    }
+
+    @Test public void testAddStudentInvalidGroupBoundaries(){
+        repo.save(new Student("1", "Johnny 110", 110));
+        assertNull("Verifying student with invalid group", repo.findOne("1"));
+
+        repo.save(new Student("1", "Johnny 938", 938));
+        assertNull("Verifying student with invalid group", repo.findOne("1"));
+
+    }
 }
